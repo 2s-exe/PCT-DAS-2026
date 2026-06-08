986ea8879be34fde4693e49885313d56ea43d64b
sow <saidou.sow@sahanalytics.com>
2026-06-08 16:43:14 +0000

    Fix: Next.js config for Vercel deployment

--- PctFrontend/next.config.js
+++ PctFrontend/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // No rewrites - frontend will call backend directly at http://backend:8000 from container
-  // or http://localhost:8000 from browser
+  output: 'standalone',
 }
 
 module.exports = nextConfig
